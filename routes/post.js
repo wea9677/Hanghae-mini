9e30f2a351157c62c279c4eff6bdcdb19327bf36
wea9677 <wea9677@naver.com>
2022-06-11 02:52:30 +0900

    patch, delete 1차 코드 추가

--- routes/post.js
+++ routes/post.js
@@ -7,13 +7,13 @@ const router = express.Router();
 
 //게시물 등록하기
 router.post("/write", async (req, res) =>{
-    const {nickName, title, content, imageUrl } = req.body;
-
+    const {title, content, imageUrl } = req.body;
+    let { user } = res.locals;
     const createPost = await Post.create({
-        nickName,
         title,
         content,
         imageUrl,
+        nickName : user.nickName,
     })
     // res.json({post : createPost});
     res.status(201).json({result: 'success', msg: '글이 등록되었습니다.'});
@@ -31,10 +31,62 @@ router.get("/main", async (req, res) =>{
 //게시물 상세조회
 
 router.get("/main/:contentId", async (req, res)=>{
-    const {contentId} =req.params;
+    const {contentId} = req.params;
     const post = await Post.findById(contentId);
+
+
     res.json({post});
-})
+});
+
+
+//게시물 수정 페이지
+
+// router.get("/main/:contentId/modify", async (req, res) => {
+//     const { contentId } = req.params;
+
+//     const post = await Post.findById(contentId);
+//     // console.log("article은 : " , article);
+//     // console.log("articleId는 : ", article.articleId);
+//     res.json({post})
+//     // res.status(200).render('write', { post: post });
+// });
+
+
+//게시물 수정
+
+router.patch("/main/:contentId/modify", async (req, res)=> {
+    const {contentId} = req.params;
+    const {title, content, imageUrl} = req.body;
+   
+    const existsPost = await Post.findById(contentId);
+   
+    const modifyPost =  await Post.findByIdAndUpdate(contentId, {
+           $set : {title:title, content:content, imageUrl : imageUrl} ,
+        });
+        res.status(201).json({
+            result:'success',
+            msg:"글이 수정되었습니다.",
+        })
+        
+});
+
+
+
+
+
+//게시물 삭제
+router.delete("/main/:contentId/modify", async (req, res)=>{
+    
+    const {contentId} = req.params;
+
+    const existsPost = await Post.findById(contentId);
+        // if(existsPost.length > 0) {
+            await Post.deleteOne({contentId});
+        //}
+    res.json({result:"success"});
+
+});
+
 
 
 module.exports = router;
