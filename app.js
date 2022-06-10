88b0e07d5a2c09a9515c84e5b6543ca105a30052
wea9677 <wea9677@naver.com>
2022-06-10 19:06:58 +0900

    post 1차 코드

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 
 const port = 3000;
 
+const postRouter = require("./routes/post");
+
 mongoose.connect('mongodb://0.0.0.0/goodnight', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -12,6 +14,13 @@ const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error'));
 
 const app = express();
+
+//body 읽기
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.use("/api", postRouter);
 
 app.use("/", (req, res) =>{
     res.send('테스트 페이지')
---
+++ routes/post.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const Post = require("../models/post");
+const User = require("../models/user");
+const router = express.Router();
+
+
+
+//게시물 등록하기
+router.post("/write", async (req, res) =>{
+    const {nickName, title, content, imageUrl } = req.body;
+
+    const createPost = await Post.create({
+        nickName,
+        title,
+        content,
+        imageUrl,
+    })
+    res.json({post : createPost});
+    // res.status(201).json({result: 'success', msg: '글이 등록되었습니다.'});
+
+})
+//게시물 조회
+router.get("/main", async (req, res) =>{
+   const contents = await Post.find().sort({createdAt : 'desc'});
+
+    res.json({contents})
+   
+});
+
+
+
+module.exports = router;
