3034edd20bb5e27c6d1212ba76ea11704e41cfa1
wea9677 <wea9677@naver.com>
2022-06-10 19:39:48 +0900

    게시물 상세조회 1차

--- routes/post.js
+++ routes/post.js
@@ -15,18 +15,26 @@ router.post("/write", async (req, res) =>{
         content,
         imageUrl,
     })
-    res.json({post : createPost});
-    // res.status(201).json({result: 'success', msg: '글이 등록되었습니다.'});
+    // res.json({post : createPost});
+    res.status(201).json({result: 'success', msg: '글이 등록되었습니다.'});
 
 })
 //게시물 조회
 router.get("/main", async (req, res) =>{
    const contents = await Post.find().sort({createdAt : 'desc'});
 
-    res.json({contents})
+    res.json({contents});
    
 });
 
+
+//게시물 상세조회
+
+router.get("/main/:contentId", async (req, res)=>{
+    const {contentId} =req.params;
+    const post = await Post.findById(contentId);
+    res.json({post});
+})
 
 
 module.exports = router;
