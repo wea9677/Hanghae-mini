504de4eee909b197fd8fb04755fb83082c6d22da
seunghyeon55 <ilralz25@naver.com>
2022-06-11 12:19:45 +0900

    first commit

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const port = 3000;
 
 
 const postRouter = require("./routes/post");
-
+const userRouter = require("./models/user")
 mongoose.connect('mongodb://0.0.0.0/goodnight', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
