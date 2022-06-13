50b1b95af92180332edcfa0c0c07f2289491c080
wea9677 <wea9677@naver.com>
2022-06-13 22:03:44 +0900

    좋아요 취소 성공

--- routes/like.js
+++ routes/like.js
@@ -7,11 +7,12 @@ const authMiddleware = require('../middlewares/auth-middleware')
 
 // 게시물 좋아요
 router.post('/post/:contentId/like', authMiddleware, async (req, res) => {
-    const {nickName} = res.locals.user;
+    const { nickName } = res.locals.user;
     const { contentId } = req.params;
 
-    const findLike = await Like.findOne({contentId});
+    const findLike = await Like.findOne({contentId, nickName});
     
+  
     if(findLike){
         res.status(400).send({ errorMessage: "이미 좋아요를 하셨습니다!"});
     }else{
@@ -19,6 +20,8 @@ router.post('/post/:contentId/like', authMiddleware, async (req, res) => {
     const like = await Like.create({
         nickName, contentId
     });
+    await like.save()
+
     res.status(201).json({ result: 'success', msg: "좋아요 완료!"});
     }
   });
@@ -31,20 +34,38 @@ router.get('/post/:contentId/like', async (req,res) => {
 });
 
 //좋아요 취소
-router.delete('/post/:contentId/like', async (req,res) =>{
-    const { nickname } = res.locals.user;
+// router.delete('/post/:contentId/unlike', async (req,res) =>{
+//     const {nickName} = res.locals.user;
+//     console.log(nickName);
+//     const { contentId } = req.params;
 
-    const findLike = await Like.findOne({nickname});
+//     const findLike = await Like.findOne({ nickName, contentId});
+   
+
+//     if(!findLike){
+//         res.status(400).send({errorMessage: "좋아요를 하지 않았습니다."});
+//     }
+//     const unlike = await Like.findByIdAndDelete(nickName);
+   
+
+
+
+    
+//     res.status(200).json({unlike, msg: "좋아요 취소 완료!"})
+// });
+
+router.delete('/post/:contentId/unlike', authMiddleware, async (req,res) =>{
+    const { nickName } = res.locals.user;
+
+    const findLike = await Like.findOne({ nickName });
 
     if(!findLike){
         res.status(400).send({errorMessage: "좋아요를 하지 않았습니다."});
     }
 
     const unLike = await Like.findByIdAndDelete(findLike);
-    res.status(200).json({result: 'success', msg: "좋아요 취소 완료!"})
+    res.status(200).json({result: 'success', msg: "좋아요 취소 완료!", unLike})
 });
-
-
 
 
 
