4ba5634f32b52024bcd27483cf630f1be941f231
wea9677 <wea9677@naver.com>
2022-06-14 15:14:39 +0900

    date 테스트2

--- routes/post.js
+++ routes/post.js
@@ -15,7 +15,7 @@ router.post("/post", authMiddleware, async (req, res) =>{
         // const  date = new date
         const {title, content, imageUrl} = req.body;
         const createPost = await Post.create({
-             title, content, imageUrl, nickName
+             title, content, imageUrl, nickName, date
         });
         // console.log(createPost);
         // console.log(nickName);
