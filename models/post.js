5592fba35b84fc989728c544697392a08a68da3c
wea9677 <wea9677@naver.com>
2022-06-14 17:42:47 +0900

    Date 테스트 6

--- models/post.js
+++ models/post.js
@@ -29,7 +29,10 @@ const PostSchema = new mongoose.Schema(
 
         },
 
-        nDate : String,
+        nDate : {
+            type:String,
+            required:true
+        }
              
            
 
